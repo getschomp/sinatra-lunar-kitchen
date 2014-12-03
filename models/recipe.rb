d5241a676c4ab0ec513c4f52789e5179ca70b597
Daniel Bonnell <daniel.bonnell@gmail.com>
2014-12-03 13:19:21 -0500

    Started working on Recipes.all method

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,2 +1,28 @@
 class Recipe
+  attr_reader :results
+  
+  def initialize
+    @results = []
+  end
+
+  def db_connection
+    begin
+      connection = PG.connect(dbname: 'recipes')
+
+      yield(connection)
+
+    ensure
+      connection.close
+    end
+  end
+
+  def self.all
+    sql = %{
+      SELECT name, id FROM recipes ORDER BY name LIMIT 20;
+    }
+
+    db_connection do |conn|
+      @results = conn.exec_params(sql)
+    end
+  end
 end
