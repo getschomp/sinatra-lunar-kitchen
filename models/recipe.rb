d103047ee2f49feb111cd2085e191cb9cffee2cb
Daniel Bonnell <daniel.bonnell@gmail.com>
2014-12-03 14:27:27 -0500

    Recipe.all method, name method, and id method working now

--- models/recipe.rb
+++ models/recipe.rb
@@ -2,24 +2,46 @@ require 'pry'
 class Recipe
   attr_reader :results
 
-  def initialize
-    @results = []
+  def initialize(name, id)
+    @name = name
+    @id = id
+  end
+
+  def self.db_connection
+    begin
+      connection = PG.connect(dbname: 'recipes')
+
+      yield(connection)
+
+    ensure
+      connection.close
+    end
   end
 
   def self.all
     sql = %{
       SELECT name, id FROM recipes ORDER BY name LIMIT 20;
     }
+    query_results = nil
+    recipes_array = []
 
-    begin
-      connection = PG.connect(dbname: 'recipes')
-      @results = connection.exec_params(sql).to_a
-    ensure
-      connection.close
+    db_connection do |conn|
+      query_results = conn.exec_params(sql).to_a
     end
+
+    query_results.each do |recipe_obj|
+      recipe_obj = Recipe.new(recipe_obj["name"], recipe_obj["id"])
+      recipes_array << recipe_obj
+    end
+    recipes_array
   end
-  binding.pry
-  def self.id
-    @results[:id]
+
+  def id
+    @id
   end
+
+  def name
+    @name
+  end
+
 end
