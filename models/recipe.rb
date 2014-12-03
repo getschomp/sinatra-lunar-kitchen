71c993d88c191411c5ee896734236d966d3f8b17
Daniel Bonnell <daniel.bonnell@gmail.com>
2014-12-03 15:05:57 -0500

    Name, id, description, and instructions methods working.

--- models/recipe.rb
+++ models/recipe.rb
@@ -2,9 +2,11 @@ require 'pry'
 class Recipe
   attr_reader :results
 
-  def initialize(name, id)
+  def initialize(name, id, description, instructions)
     @name = name
     @id = id
+    @description = description
+    @instructions = instructions
   end
 
   def self.db_connection
@@ -20,7 +22,7 @@ class Recipe
 
   def self.all
     sql = %{
-      SELECT name, id FROM recipes ORDER BY name LIMIT 20;
+      SELECT name, id, description, instructions, FROM recipes ORDER BY name LIMIT 20;
     }
     query_results = nil
     recipes_array = []
@@ -30,7 +32,7 @@ class Recipe
     end
 
     query_results.each do |recipe_obj|
-      recipe_obj = Recipe.new(recipe_obj["name"], recipe_obj["id"])
+      recipe_obj = Recipe.new(recipe_obj["name"], recipe_obj["id"], recipe_obj["description"], recipe_obj["instructions"])
       recipes_array << recipe_obj
     end
     recipes_array
@@ -42,6 +44,26 @@ class Recipe
 
   def name
     @name
+  end
+
+  def description
+    @description
+  end
+
+  def instructions
+    @instructions
+  end
+
+  def self.find(id)
+    sql = %{
+      SELECT name, id, description, instructions FROM recipes WHERE id = #{id};
+    }
+    recipe = nil
+
+    db_connection do |conn|
+      recipe = conn.exec_params(sql).to_a
+    end
+    recipe = Recipe.new(recipe[0]["name"], recipe[0]["id"], recipe[0]["description"], recipe[0]["instructions"])
   end
 
 end
